acca7808a5f2af40f7ebaf73e2531a3cce1c9f97
MoonCheung <ikmoons.is@gmail.com>
2025-10-16 16:05:14 +0800

    chore: updated add page

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,22 +1,130 @@
-import crxLogo from '@/assets/img/crx.svg'
-import reactLogo from '@/assets/img/react.svg'
-import viteLogo from '@/assets/img/vite.svg'
-import HelloWorld from '@/components/HelloWorld'
+import { useState, useEffect } from "react";
+import { nanoid } from 'nanoid'
+import { printDate } from '@/lib/helper';
+import TodoForm from '@/components/TodoForm';
+import TodoList from "@/components/TodoList";
 import './App.css'
 
 export default function App() {
+  const currDate = new Date();
+  const date = printDate(currDate);
+
+  const [editing, setEditing] = useState(false);
+
+  const [currentTodo, setCurrentTodo] = useState({
+    id: null, description: '', completed: null
+  })
+
+  const todosData: any = [
+    //{id: uuidv4(), description: 'Go shopping', completed: false}
+  ]
+
+  const [todos, setTodos] = useState(todosData);
+
+  // This function stores todos in localStorage:
+  useEffect(() => {
+    const getLocalTodos = () => {
+      if(localStorage.getItem('todos') === null){
+        localStorage.setItem('todos', JSON.stringify([]))
+      }else{
+        setTodos(JSON.parse(localStorage.getItem("todos")))
+      }
+    }
+    getLocalTodos();
+  }, [])
+
+  const [status, setStatus] = useState('all');
+  // We need a empty array where store the filtered 'todos'
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    // depending on the status, show me a TodoList. filteredTodos will be sent to TodoList component.
+    const filterHandler = () => {
+      switch(status) {
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true));
+          break;
+        case 'Uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default: 
+          setFilteredTodos(todos)
+          break;
+      }
+    }
+
+    // Save to localStorage
+    const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  const addTodo = (todo) => {
+    todo.id = nanoid();
+    todo.completed = false;
+    setTodos([
+      todo,
+      ...todos,
+    ])
+  }
+
+  //remove todos:
+  // we send an id and we put the 'todo' out which match with the sent id
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const editTodo = (todo) => {
+    //the EditTodoForm component is showed
+    setEditing(true);
+    // and the clicked todo's info is stored in the currentTodo variable 
+    setCurrentTodo({
+      id: todo.id, description: todo.description, completed: todo.completed
+    })
+  }
+
+  const updateTodo = (id, updatedTodo) => {
+    setEditing(false);
+    setTodos(todos.map(todo => todo.id === id ? updatedTodo : todo))
+  }
+
+  const completeTodo = (todo) => {
+    console.log('completed todo:', todo)
+    setTodos(todos.map(item => {
+      if (item.id === todo.id) {
+        return { ...item, completed: !item.completed }
+      }
+      return item;
+    }));
+  }
+  
+
   return (
-    <div>
-      <a href="https://vite.dev" target="_blank" rel="noreferrer">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-      <a href="https://crxjs.dev/vite-plugin" target="_blank" rel="noreferrer">
-        <img src={crxLogo} className="logo crx" alt="crx logo" />
-      </a>
-      <HelloWorld msg="Vite + React + CRXJS" />
-    </div>
+    <main className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-end items-start pl-6 bg-[linear-gradient(rgba(0,_0,_0,_0.0),_rgba(0,_0,_0,_0.5)),_url(../images/cold-night.jpg)_no-repeat_right_bottom] bg-cover w-full min-h-[25vh] rounded-tl-[8px] rounded-br-[0] rounded-tr-[8px] rounded-bl-[0]">
+        <h2>My Day</h2>
+        <p className="text-[1rem] mt-2 pb-6">{date}</p>
+      </div>
+      <TodoForm 
+        editing={editing}
+        todos={addTodo}
+        setStatus={setStatus}
+        currentTodo={currentTodo}
+        updateTodo={updateTodo}
+       />
+      <TodoList 
+        todos={todos} 
+        filteredTodos={filteredTodos} 
+        deleteTodo={deleteTodo}
+        completeTodo={completeTodo}
+        editTodo={editTodo}
+      />
+      <footer>
+        <p>&copy; 2025 MoonCheung Author &bull; <a href="https://github.com/MoonCheung" target="_blank" rel="noreferrer">Portfolio</a></p>
+      </footer>
+    </main>
   )
 }
