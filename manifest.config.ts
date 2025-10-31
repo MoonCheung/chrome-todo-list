fcc5ca6d0750deaaec4efb94cd984d492ae2f957
MoonCheung <ikmoons.is@gmail.com>
2025-10-31 23:01:29 +0800

    chore: add image jpg

--- manifest.config.ts
+++ manifest.config.ts
@@ -25,4 +25,10 @@ export default defineManifest({
   side_panel: {
     default_path: 'src/sidepanel/index.html',
   },
+  web_accessible_resources: [
+    {
+      resources: ['src/assets/img/*'],
+      matches: ['<all_urls>']
+    }
+  ]
 })
